92560e7d361d6e61f6ac22137dd3e35d1ee67c96
Yvon Quemener <yvon.quemener@csdps.qc.ca>
2020-10-30 17:07:37 -0400

    r11

--- src/version.php
+++ src/version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = 2020082500;
+$plugin->version = 2020103000;
 
 // This is the named version.
-$plugin->release = 'R10-V1.9.2';  
+$plugin->release = 'R11-V1.11.0';  
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;                                                                                                    
@@ -43,5 +43,5 @@ $plugin->component = 'theme_recit_science';
  
 // This is a list of plugins, this plugin depends on (and their versions).                                                          
 $plugin->dependencies = [                                                                                                           
-    'theme_recit' => 2020082500                                                                                                   
+    'theme_recit' => 2020103000                                                                                                   
 ];
